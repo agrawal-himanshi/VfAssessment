9bb808eb7b7d45e1db4ade2b547ce83e62ae26bf
himanshi <himanshiagrawal331@gmal.com>
2024-11-13 18:47:13 +0530

    commit changes

--- force-app/main/default/lwc/boxIntegration/boxIntegration.js
+++ force-app/main/default/lwc/boxIntegration/boxIntegration.js
@@ -1,6 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import boxIconResource from '@salesforce/resourceUrl/boxIconResource';
 
 export default class BoxIntegration extends LightningElement {
     myCustomIconUrl = boxIconResource;
+    lastClickedDiv = null;  
+    @track path = [{ label: 'Home', value: 'root' }];
+
+    handleDivClick(event) {
+        const div = event.currentTarget;
+        if (this.lastClickedDiv && this.lastClickedDiv !== div) {
+            this.lastClickedDiv.classList.remove('highlighted');
+        }
+        div.classList.toggle('highlighted');
+        this.lastClickedDiv = div;
+        console.log("Div clicked, performing action...");
+    }
 }
